1a618c072d7aa4c3be2d888df9b24febe9010c9e
Kajanthan <kajanthansureshkumar@gmail.com>
2021-09-24 20:02:44 +0530

    Footer Added

--- product_page.php
+++ product_page.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-	
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
 </head>
 <body>
     <!-- Header -->
@@ -475,7 +475,54 @@
 			</div>
 		</div>
 	</section>
-
+	<!-- Footer Section Begin -->
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-6">
+					<div class="footer_about">
+						<div class="footer_logo">
+							<a href="#"><img src="images/logo2.png" alt=""></a>
+						</div>
+						<p>Passion shouldn't cost a fortune. <br> On MiCROTECH, shop online for better deals on Electronic products.</p>
+						<a href="#"><img src="images/payment_icons.png" alt=""></a>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-3 col-sm-6">
+					<div class="footer_widget">
+						<h6>Shopping</h6>
+						<ul>
+							<li><a href="#">Home Page</a></li>
+							<li><a href="#">Browse Products</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-3 col-sm-6">
+					<div class="footer_widget">
+						<h6>Users</h6>
+						<ul>
+							<li><a href="#">Admin Login</a></li>
+							<li><a href="#">Buyers Login</a></li>
+							<li><a href="#">Registration</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+					<div class="footer_widget">
+						<h6>NewsLetter</h6>
+						<div class="footer_newsletter">
+							<p>Be the first to know about new arrivals, sales & promos!</p>
+							<form action="#">
+								<input type="text" placeholder="Your email">
+								<button type="submit"><span class="icon_mail_alt"></span></button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
