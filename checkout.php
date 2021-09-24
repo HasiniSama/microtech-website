fd9c308c685394e776db457abaad77fa8688ab5a
pasandevin <jayawardene.pasandevin@gmail.com>
2021-09-24 22:05:32 +0530

    removed the payment info form

--- checkout.php
+++ checkout.php
@@ -124,7 +124,6 @@
 <!--Breadkcrumb end-->
 
 
-
 <!--Form begin-->
 <div class="container container-form">
 
@@ -140,32 +139,32 @@
                         <h6 class="my-0">Product name</h6>
                         <small class="text-muted">Brief description</small>
                     </div>
-                    <span class="text-muted">$12</span>
+                    <span class="text-muted">LKR 120000</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-condensed">
                     <div>
                         <h6 class="my-0">Second product</h6>
                         <small class="text-muted">Brief description</small>
                     </div>
-                    <span class="text-muted">$8</span>
+                    <span class="text-muted">LKR 200000</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-condensed">
                     <div>
                         <h6 class="my-0">Third item</h6>
                         <small class="text-muted">Brief description</small>
                     </div>
-                    <span class="text-muted">$5</span>
+                    <span class="text-muted">LKR 500000</span>
                 </li>
-                <li class="list-group-item d-flex justify-content-between bg-light">
+                <li class="list-group-item d-flex justify-content-between bg-light text-success">
                     <div class="text-success">
-                        <h6 class="my-0">Promo code</h6>
-                        <small>EXAMPLECODE</small>
+                        <h6 class="my-0 textblue">Promo code</h6>
+                        <small class="textblue">EXAMPLECODE</small>
                     </div>
-                    <span class="text-success">-$5</span>
+                    <span class="textblue">-LKR 50000</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between">
-                    <span>Total (USD)</span>
-                    <strong>$20</strong>
+                    <span>Total (LKR)</span>
+                    <strong>LKR 770000</strong>
                 </li>
             </ul>
 
@@ -185,23 +184,14 @@
                         <div class="invalid-feedback"> Valid last name is required.</div>
                     </div>
                 </div>
+
                 <div class="mb-3">
-                    <label for="username">Username</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">@</span>
-                        </div>
-                        <input type="text" class="form-control" id="username" placeholder="Username" required="">
-                        <div class="invalid-feedback" style="width: 100%;"> Your username is required.</div>
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="email">Email <span class="text-muted">(Optional)</span></label>
+                    <label for="email">Email <span class="text-muted">(Required)</span></label>
                     <input type="email" class="form-control" id="email" placeholder="you@example.com">
                     <div class="invalid-feedback"> Please enter a valid email address for shipping updates.</div>
                 </div>
                 <div class="mb-3">
-                    <label for="address">Address</label>
+                    <label for="address">Street adress 1</label>
                     <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
                     <div class="invalid-feedback"> Please enter your shipping address.</div>
                 </div>
@@ -210,29 +200,32 @@
                     <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
                 </div>
                 <div class="row">
+                    <div class="col-md-4 mb-3">
+                        <label for="city">City</label>
+                        <select class="custom-select d-block w-100" id="city" required="">
+                            <option value="">Choose...</option>
+                            <option>Colombo</option>
+                            <option>Kaluthara</option>
+                        </select>
+                        <div class="invalid-feedback"> Please provide a valid state.</div>
+                    </div>
                     <div class="col-md-5 mb-3">
                         <label for="country">Country</label>
                         <select class="custom-select d-block w-100" id="country" required="">
                             <option value="">Choose...</option>
+                            <option>Sri Lanka</option>
                             <option>United States</option>
                         </select>
                         <div class="invalid-feedback"> Please select a valid country.</div>
                     </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="state">State</label>
-                        <select class="custom-select d-block w-100" id="state" required="">
-                            <option value="">Choose...</option>
-                            <option>California</option>
-                        </select>
-                        <div class="invalid-feedback"> Please provide a valid state.</div>
-                    </div>
+
                     <div class="col-md-3 mb-3">
                         <label for="zip">Zip</label>
                         <input type="text" class="form-control" id="zip" placeholder="" required="">
                         <div class="invalid-feedback"> Zip code required.</div>
                     </div>
                 </div>
-                <hr class="mb-4">
+
                 <div class="custom-control custom-checkbox">
                     <input type="checkbox" class="custom-control-input" id="same-address">
                     <label class="custom-control-label" for="same-address">Shipping address is the same as my billing
@@ -242,60 +235,15 @@
                     <input type="checkbox" class="custom-control-input" id="save-info">
                     <label class="custom-control-label" for="save-info">Save this information for next time</label>
                 </div>
+
+
                 <hr class="mb-4">
-                <h4 class="mb-3">Payment</h4>
-                <div class="d-block my-3">
-                    <div class="custom-control custom-radio">
-                        <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked=""
-                               required="">
-                        <label class="custom-control-label" for="credit">Credit card</label>
-                    </div>
-                    <div class="custom-control custom-radio">
-                        <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="">
-                        <label class="custom-control-label" for="debit">Debit card</label>
-                    </div>
-                    <div class="custom-control custom-radio">
-                        <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required="">
-                        <label class="custom-control-label" for="paypal">PayPal</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="cc-name">Name on card</label>
-                        <input type="text" class="form-control" id="cc-name" placeholder="" required="">
-                        <small class="text-muted">Full name as displayed on card</small>
-                        <div class="invalid-feedback"> Name on card is required</div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="cc-number">Credit card number</label>
-                        <input type="text" class="form-control" id="cc-number" placeholder="" required="">
-                        <div class="invalid-feedback"> Credit card number is required</div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3 mb-3">
-                        <label for="cc-expiration">Expiration</label>
-                        <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
-                        <div class="invalid-feedback"> Expiration date required</div>
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="cc-cvv">CVV</label>
-                        <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
-                        <div class="invalid-feedback"> Security code required</div>
-                    </div>
-                </div>
-                <hr class="mb-4">
-                <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+                <button class="btn btn-dark btn-lg btn-block" type="submit">Continue to checkout</button>
             </form>
         </div>
     </div>
-    <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">© 2017-2019 Company Name</p>
-        <ul class="list-inline">
-            <li class="list-inline-item"><a href="#">Privacy</a></li>
-            <li class="list-inline-item"><a href="#">Terms</a></li>
-            <li class="list-inline-item"><a href="#">Support</a></li>
-        </ul>
+    <footer class="my-2 pt-5 text-muted text-center text-small">
+
     </footer>
 </div>
 <!--Form end-->
@@ -352,8 +300,17 @@
 <!--Footer end-->
 
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
 
 <script src="js/checkout.js"></script>
