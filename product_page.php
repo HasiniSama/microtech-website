59ea33c297d5309a5b91f6ee374aaf12d102cceb
Kajanthan <kajanthansureshkumar@gmail.com>
2021-09-24 19:49:25 +0530

    Breadcrumb added

--- product_page.php
+++ product_page.php
@@ -43,7 +43,7 @@
 			</div>
 
             <!-- Menu bar -->
-			<div class="wrap-menu">
+			<div class="wrap-menu how-shadow1">
 				<nav class="limiter-menu container">
 					
 					<!-- Logo -->		
@@ -100,11 +100,31 @@
 		</div>
 	</header>
 
+    <!-- breadcrumb -->
+    <div class="container">
+        <div class="bread-crumb flex-w  p-r-15 p-t-100">
+            <a href="index.php" class="stext-109 cl8 hov-cl1 trans-04">
+                Shop
+                <i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
+            </a>
+
+            <a href="shopping_page.php" class="stext-109 cl8 hov-cl1 trans-04">
+                Smartphones
+                <i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
+            </a>
+
+            <span class="stext-109 cl4">
+                Samsung Galaxy Note20 5G
+            </span>
+        </div>
+    </div>
+
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="js/app.js"></script>	
+	<script src="https://kit.fontawesome.com/bfc2cbc6c6.js" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>	
 
 </body>
 </html>
