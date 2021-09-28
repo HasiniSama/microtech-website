e0a7445d3ff836b6a7cbe4ff7cace9678c671a2c
Hasini Samarathunga <hasinisama99@gmail.com>
2021-09-28 23:16:42 +0530

    SQL Updated

--- microtech.sql
+++ microtech.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 28, 2021 at 04:10 PM
--- Server version: 5.7.31
--- PHP Version: 7.4.9
+-- Host: localhost
+-- Generation Time: Sep 28, 2021 at 07:45 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,15 +27,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `address`
 --
 
-DROP TABLE IF EXISTS `address`;
-CREATE TABLE IF NOT EXISTS `address` (
+CREATE TABLE `address` (
   `usermail` varchar(50) NOT NULL,
   `addr_line1` varchar(200) NOT NULL,
   `addr_line2` varchar(200) NOT NULL,
   `city` varchar(20) NOT NULL,
   `country` varchar(20) NOT NULL,
-  `zip` int(7) NOT NULL,
-  PRIMARY KEY (`usermail`)
+  `zip` int(7) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -51,13 +49,11 @@ INSERT INTO `address` (`usermail`, `addr_line1`, `addr_line2`, `city`, `country`
 -- Table structure for table `admins`
 --
 
-DROP TABLE IF EXISTS `admins`;
-CREATE TABLE IF NOT EXISTS `admins` (
+CREATE TABLE `admins` (
   `email` varchar(50) NOT NULL,
   `passwd` varchar(40) NOT NULL,
   `f_name` varchar(30) DEFAULT NULL,
-  `l_name` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`email`)
+  `l_name` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -76,21 +72,11 @@ INSERT INTO `admins` (`email`, `passwd`, `f_name`, `l_name`) VALUES
 -- Table structure for table `cart`
 --
 
-DROP TABLE IF EXISTS `cart`;
-CREATE TABLE IF NOT EXISTS `cart` (
+CREATE TABLE `cart` (
   `usermail` varchar(50) NOT NULL,
   `itemid` varchar(10) NOT NULL,
-  `no_of_items` int(4) NOT NULL,
-  PRIMARY KEY (`usermail`,`itemid`)
+  `no_of_items` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `cart`
---
-
-INSERT INTO `cart` (`usermail`, `itemid`, `no_of_items`) VALUES
-('hasinisama99@gmail.com', '02', 4),
-('hasinisama99@gmail.com', '03', 3);
 
 -- --------------------------------------------------------
 
@@ -98,8 +84,7 @@ INSERT INTO `cart` (`usermail`, `itemid`, `no_of_items`) VALUES
 -- Table structure for table `items`
 --
 
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE `items` (
   `item_id` varchar(10) NOT NULL,
   `item_name` varchar(100) NOT NULL,
   `category` varchar(20) NOT NULL,
@@ -109,8 +94,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `img_name2` varchar(100) DEFAULT NULL,
   `img_name3` varchar(100) DEFAULT NULL,
   `img_name4` varchar(100) DEFAULT NULL,
-  `long_description` varchar(4000) NOT NULL,
-  PRIMARY KEY (`item_id`)
+  `long_description` varchar(4000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -133,13 +117,21 @@ INSERT INTO `items` (`item_id`, `item_name`, `category`, `brand`, `item_price`, 
 -- Table structure for table `orderdetails`
 --
 
-DROP TABLE IF EXISTS `orderdetails`;
-CREATE TABLE IF NOT EXISTS `orderdetails` (
-  `orderid` varchar(10) NOT NULL,
-  `itemid` varchar(10) NOT NULL,
-  `no_of_items` int(4) NOT NULL,
-  PRIMARY KEY (`orderid`,`itemid`)
+CREATE TABLE `orderdetails` (
+  `order_id` varchar(10) NOT NULL,
+  `item_id` varchar(10) NOT NULL,
+  `no_of_items` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orderdetails`
+--
+
+INSERT INTO `orderdetails` (`order_id`, `item_id`, `no_of_items`) VALUES
+('001', '01', 1),
+('002', '03', 1),
+('003', '04', 1),
+('004', '07', 2);
 
 -- --------------------------------------------------------
 
@@ -147,14 +139,22 @@ CREATE TABLE IF NOT EXISTS `orderdetails` (
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `orderid` varchar(10) NOT NULL,
-  `emai` varchar(50) NOT NULL,
-  `price` int(7) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`orderid`)
+CREATE TABLE `orders` (
+  `order_id` varchar(10) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `revenue` int(7) NOT NULL,
+  `order_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_email`, `revenue`, `order_time`) VALUES
+('001', 'hasinisama99@gmail.com', 174990, '2021-09-28 17:41:43'),
+('002', 'hasinisama99@gmail.com', 52199, '2021-09-28 17:41:43'),
+('003', 'hasinisama99@gmail.com', 74999, '2021-09-28 17:41:43'),
+('004', 'hasinisama99@gmail.com', 14380, '2021-09-28 17:41:43');
 
 -- --------------------------------------------------------
 
@@ -162,11 +162,9 @@ CREATE TABLE IF NOT EXISTS `orders` (
 -- Table structure for table `stock`
 --
 
-DROP TABLE IF EXISTS `stock`;
-CREATE TABLE IF NOT EXISTS `stock` (
+CREATE TABLE `stock` (
   `item_id` varchar(10) NOT NULL,
-  `no_of_items` int(3) NOT NULL,
-  PRIMARY KEY (`item_id`)
+  `no_of_items` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -175,16 +173,14 @@ CREATE TABLE IF NOT EXISTS `stock` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `f_name` varchar(30) NOT NULL,
   `l_name` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email_verification_link` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`email`)
+  `status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -193,6 +189,58 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`email`, `f_name`, `l_name`, `password`, `email_verification_link`, `email_verified_at`, `status`) VALUES
 ('hasinisama99@gmail.com', 'Hasini', 'Samarathunga', '81dc9bdb52d04dc20036dbd8313ed055', '6008ad9b551eba21f097ba66b63f3bc53631', '2021-09-26 16:58:28', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`usermail`);
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`usermail`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`order_id`,`item_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
